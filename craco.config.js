13f0f8cb6162e9bd41441f8b8686effd03c80fb9
Isaac Henrique <isaac@isaacsolutions.tech>
2024-05-29 20:10:30 -0300

    feat: config env file

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,18 @@
 const fs = require("fs");
 const path = require("path");
+const envs = require("dotenv").config();
+const { DefinePlugin } = require("webpack");
+
+class IgnoreWarningsPlugin {
+  apply(compiler) {
+    compiler.hooks.afterCompile.tap("IgnoreWarningsPlugin", (compilation) => {
+      compilation.warnings = compilation.warnings.filter(
+        (warning) =>
+          !/Conflicting values for 'process.env'/.test(warning.message)
+      );
+    });
+  }
+}
 
 function createAliases(baseDir) {
   const srcPath = path.resolve(__dirname, baseDir);
@@ -16,6 +29,12 @@ function createAliases(baseDir) {
 module.exports = {
   webpack: {
     alias: createAliases("src"),
+    plugins: [
+      new DefinePlugin({
+        "process.env": JSON.stringify(envs.parsed || {}),
+      }),
+      new IgnoreWarningsPlugin(),
+    ],
   },
   typescript: {
     enableTypeChecking: true,
--- src/config/constants.config.ts
+++ src/config/constants.config.ts
@@ -1,2 +1,3 @@
-export const GITHUB_API_URL = process.env.GITHUB_API_URL ?? "https://api.github.com/graphql";
+export const GITHUB_API_URL =
+  process.env.GITHUB_API_URL ?? "https://api.github.com/graphql";
 export const GITHUB_API_KEY = process.env.GITHUB_API_KEY ?? "";
